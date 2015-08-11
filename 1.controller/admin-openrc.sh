e783eb8ea9e64d37fe026c3df4a8e656d4d26c9b
khanhnnvn <khanhnnvn@gmail.com>
2015-08-11 13:57:37 +0700

    Edit some info

--- 1.controller/admin-openrc.sh
+++
@@ -1,4 +0,0 @@
-export OS_USERNAME=admin
-export OS_PASSWORD=Welcome123
-export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://10.10.10.71:35357/v2.0
--- 1.controller/ctl-5-creatusetenant.sh
+++ 1.controller/ctl-5-creatusetenant.sh
@@ -21,7 +21,7 @@ get_id () {
 
 
 # openstack project create --description "Admin Project" admin
-# openstack user create --name=admin  --pass=Welcome123 --email=congtt@vietstack.vn
+# openstack user create --name=admin  --pass=Welcome123 --email=congtt@pythonvietnam.com
 
 #################
 
@@ -34,8 +34,8 @@ DEMO_TENANT=$(get_id keystone tenant-create --name=$DEMO_TENANT_NAME)
 INVIS_TENANT=$(get_id keystone tenant-create --name=$INVIS_TENANT_NAME)
 
 # Users
-ADMIN_USER=$(get_id keystone user-create --name="$ADMIN_USER_NAME" --pass="$ADMIN_PASS" --email=congtt@vietstack.vn)
-DEMO_USER=$(get_id keystone user-create --name="$DEMO_USER_NAME" --pass="$ADMIN_PASS" --email=congtt@vietstack.vn)
+ADMIN_USER=$(get_id keystone user-create --name="$ADMIN_USER_NAME" --pass="$ADMIN_PASS" --email=congtt@pythonvietnam.com)
+DEMO_USER=$(get_id keystone user-create --name="$DEMO_USER_NAME" --pass="$ADMIN_PASS" --email=congtt@pythonvietnam.com)
 
 # Roles
 ADMIN_ROLE=$(get_id keystone role-create --name="$ADMIN_ROLE_NAME")
@@ -54,22 +54,22 @@ keystone user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $
 keystone user-role-add --user-id $DEMO_USER --role-id $MEMBER_ROLE --tenant-id $INVIS_TENANT
 
 # Configure service users/roles
-NOVA_USER=$(get_id keystone user-create --name=nova --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=nova@vietstack.vn)
+NOVA_USER=$(get_id keystone user-create --name=nova --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=nova@pythonvietnam.com)
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $NOVA_USER --role-id $ADMIN_ROLE
 
-GLANCE_USER=$(get_id keystone user-create --name=glance --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=glance@vietstack.vn)
+GLANCE_USER=$(get_id keystone user-create --name=glance --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=glance@pythonvietnam.com)
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $GLANCE_USER --role-id $ADMIN_ROLE
 
-SWIFT_USER=$(get_id keystone user-create --name=swift --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=swift@vietstack.vn)
+SWIFT_USER=$(get_id keystone user-create --name=swift --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=swift@pythonvietnam.com)
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $SWIFT_USER --role-id $ADMIN_ROLE
 
 RESELLER_ROLE=$(get_id keystone role-create --name=ResellerAdmin)
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $NOVA_USER --role-id $RESELLER_ROLE
 
-NEUTRON_USER=$(get_id keystone user-create --name=neutron --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=neutron@vietstack.vn)
+NEUTRON_USER=$(get_id keystone user-create --name=neutron --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=neutron@pythonvietnam.com)
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $NEUTRON_USER --role-id $ADMIN_ROLE
 
-CINDER_USER=$(get_id keystone user-create --name=cinder --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=cinder@vietstack.vn)
+CINDER_USER=$(get_id keystone user-create --name=cinder --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=cinder@pythonvietnam.com)
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $CINDER_USER --role-id $ADMIN_ROLE
 
 echo "########## Begin create ENDPOINT for OPS service ########## "
